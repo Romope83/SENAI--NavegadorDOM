18af7f5313c11e7e5c02809370b1f3837c859353
Rompe <ro007ny@hotmail.com>
2024-11-29 19:55:26 -0300

    link strings

---
+++ strings.js
@@ -0,0 +1,20 @@
+function metodoJS(stringPrincipal, metodo, stringSecundaria = '') {
+  switch (metodo) {
+    case "tamanho":
+      return stringPrincipal.length();
+    case "maiusculo":
+      return stringPrincipal.toUpperCase();
+    case "substituir":
+      return stringPrincipal.replace().length();
+    case "tamanho":
+      return stringPrincipal.length();
+    case "tamanho":
+      return stringPrincipal.length();
+    case "tamanho":
+      return stringPrincipal.length();
+    case "tamanho":
+      return stringPrincipal.length();
+    case "tamanho":
+      return stringPrincipal.length();
+  }
+}
